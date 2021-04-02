39594fa6b94882dc6e77a8192c71004d2a0ef7c5
Zony Yu <zony249@gmail.com>
2021-04-01 22:48:36 -0600

    first commit

---
+++ server/cli.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string.h>
+#include <list>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+int main()
+{
+
+	int sock_desc_client;
+	struct sockaddr_in server_address;
+
+	if ((sock_desc_client = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		cerr << "Error Creating Socket" << endl;
+		return 0;
+	}
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(8888);
+
+	if(inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0)
+	{
+		cerr << "Invalid Address" << endl;
+		return 0;
+	}
+
+	if (connect(sock_desc_client, (sockaddr *) &server_address, sizeof(server_address)) < 0)
+	{
+		cerr << "Connection Failed" << endl;
+		return 0;
+	}
+
+	string line;
+	while (getline (cin, line))
+	{
+		send(sock_desc_client, line.c_str(), strlen(line.c_str()), 0);
+		cout << "Message Sent" << endl;
+	}
+
+	return 0;
+}
